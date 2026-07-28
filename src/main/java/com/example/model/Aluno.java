09cd9da57148f5223a02b3a0eb37c057c76ebc7a
leonardoIFBA <162935511+leonardoIFBA@users.noreply.github.com>
2026-07-27 22:16:17 -0300

    Update Aluno.java

--- src/main/java/com/example/model/Aluno.java
+++ src/main/java/com/example/model/Aluno.java
@@ -13,8 +13,7 @@ public class Aluno{
     private String email;
     private int idade;
     private String cpf;
-    //private Curso curso;
-
+    
     /*** Construtor vazio e com parametros, auxiliam na criação dos objetos alunos */
     public Aluno(){}
 
@@ -33,66 +32,9 @@ public class Aluno{
         //this.curso = curso;
     }
 
-
-
-    /**** Metodos getters e Setters dos atributos (Encapsulamento) */
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public String getCpf(){
-        return cpf;
-    }
-
-    public void setCpf(String cpf){
-        this.cpf = cpf;
-    }
-
-    /*public Curso getCurso() {
-        return curso;
-    }
-
-    public void setCurso(Curso curso) {
-        this.curso = curso;
-    }*/
-
     /**** Implementação concreta do métodoo herdado da classe Pessoa  */
     public double calcularBonus() {
         return 1000 * 1.10;
     }
-
-    /**** permite visualiza as informações do objeto Aluno */
-    public String toString() {
-        return "id=" + id + ", nome=" + nome + ", email=" + email + ", idade=" + idade + ", cpf=" + cpf + ", curso=";// + curso.getNome() ;
-    }
-
     
 }
